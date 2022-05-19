976a26104aa152730fd2ded3d8246148243d69ac
jaysnel <jaylan210@gmail.com>
2022-05-19 15:37:36 -0400

    writing to contract using console.log. Need to import differently than what was showed

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "../node_modules/hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Im a smart contract!");
+    }
+}
