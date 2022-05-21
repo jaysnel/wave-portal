a860be791dcd7550198b8d2f1d59ed2ec506f186
jaysnel <jaylan210@gmail.com>
2022-05-21 15:05:12 -0400

    saving to contract. Showing amount of waves

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,24 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy()
-    await waveContract.deployed()
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+
     console.log("Contract deployed to:", waveContract.address);
+    console.log('Contract deployed by:', owner.address);
+
+    let totalWaves;
+    totalWaves = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    totalWaves = await waveContract.getTotalWaves();
+
+    waveTxn = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait();
+
+    totalWaves = await waveContract.getTotalWaves();
   };
   
 const runMain = async () => {
